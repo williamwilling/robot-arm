71280454eba21ae69dad8ac869d29a80019d881e
William Willing <william@williamwilling.com>
2014-05-19 20:48:19 +0200

    The robot arm gets drawn.

--- robot_arm.lua
+++ robot_arm.lua
@@ -8,7 +8,19 @@ local frame = wx.wxFrame(
   wx.wxSize(450, 450),
   wx.wxDEFAULT_FRAME_STYLE)
 
+local function paint()
+  local dc = wx.wxPaintDC(frame)
+  
+  dc:DrawLine(25, 0, 25, 10)
+  dc:DrawLine(5, 10, 45, 10)
+  dc:DrawLine(5, 10, 5, 20)
+  dc:DrawLine(45, 10, 45, 20)
+  
+  dc:delete()
+end
+
 frame:Show(true)
+frame:Connect(wx.wxEVT_PAINT, paint)
 
 robot_arm = {}
 
