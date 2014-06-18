05136936d28c8987331c137885cc18939da1af9c
Joost Ronkes Agerbeek <raj@davinci.nl>
2014-06-18 10:38:28 +0200

    Added a few extra exercises.

--- robot_arm.lua
+++ robot_arm.lua
@@ -302,6 +302,8 @@ function robot_arm:wait(seconds)
 end
 
 -- Predefined levels.
+math.randomseed(os.time())
+
 local levels = {
   ['exercise 1'] = { {}, { 'red' } },
   ['exercise 2'] = { { 'blue' }, {}, {}, {}, { 'blue' }, {}, {}, { 'blue' } },
@@ -313,12 +315,45 @@ local levels = {
   ['exercise 9'] = { { 'blue' }, { 'green', 'green' }, { 'white', 'white', 'white' }, { 'red', 'red', 'red', 'red' } },
   ['exercise 10'] = { { 'green' }, { 'blue' }, { 'white' }, { 'red' }, { 'blue' } },
   ['exercise 11'] = function()
-    --math.randomseed(os.time())
-    
-    for i  = 1, station_count - 1 do
+    for i = 2, station_count do
       local colors = { 'red', 'green', 'white', 'blue' }
       local color = colors[math.random(#colors)]
       robot_arm.assembly_line[i] = { color }
+    end
+  end,
+  ['exercise 12'] = function()
+    for i = 1, station_count - 1 do
+      local colors = { 'red', 'green', 'white', 'blue' }
+      local color = colors[math.random(#colors)]
+      robot_arm.assembly_line[i] = { color }
+    end
+  end,
+  ['exercise 13'] = function()
+    for i = 1, station_count do
+      local colors = { 'red', 'green', 'white', 'blue' }
+      local color = colors[math.random(#colors)]
+      robot_arm.assembly_line[i] = { color }
+    end
+  end,
+  ['exercise 14'] = function()
+    for i = 1, station_count do
+      robot_arm.assembly_line[i] = {}
+      
+      for _ = 1, math.random(3) do
+        local colors = { 'red', 'green', 'white', 'blue' }
+        local color = colors[math.random(#colors)]
+        table.insert(robot_arm.assembly_line[i], color)
+      end
+    end
+  end,
+  ['exercise 15'] = function()
+    for i = 1, station_count do
+      local colors = { 'red', 'green', 'white', 'blue', 'none', 'none' }
+      local color = colors[math.random(#colors)]
+      
+      if color ~= 'none' then
+        robot_arm.assembly_line[i] = { color }
+      end
     end
   end
 }
@@ -391,4 +426,4 @@ frame:Connect(wx.wxEVT_TIMER, function()
 end)
 
 wx.wxGetApp():MainLoop()
-os.exit()
+--os.exit()
